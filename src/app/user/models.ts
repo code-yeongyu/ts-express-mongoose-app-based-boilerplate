63f28c4e3febe53adf1e3b03f2c3e1a90ec7179b
Ubuntu <yeongyu@Development.kn05cl4dth3etbf33ctbtl2glh.syx.internal.cloudapp.net>
2020-11-25 14:17:46 +0000

    Update user exports

--- src/app/user/models.ts
+++ src/app/user/models.ts
@@ -1,15 +1,18 @@
 import { Schema, model, Document } from "mongoose"
 
-export interface UserModel extends Document {
+interface UserModel {
     username: string
     password: string
 }
 
-const userSchema: Schema<UserModel> = new Schema({
+interface UserModelDocument extends Document, UserModel {}
+
+const userSchema: Schema<UserModelDocument> = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 })
 
 userSchema.index({ username: 1 })
 
-export const User = model<UserModel>("User", userSchema)
+const User = model<UserModelDocument>("User", userSchema)
+export { UserModel, UserModelDocument, User }
