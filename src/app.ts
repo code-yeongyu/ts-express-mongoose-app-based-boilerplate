2c9136c6a3548ce9ea02e48101832c5519769d6b
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-12-02 01:51:43 +0900

    Fix urlencoded not working

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ passportConfig()
 
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV !== "test") {
     app.use(morgan("combined"))
